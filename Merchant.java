ba14e9d273f20cc78a538e5bf76b545e66ae0a21
Fikriya Rachma Dewi <103646257+fikriyarachma@users.noreply.github.com>
2022-04-17 13:28:06 +0700

    Update Merchant.java

--- Merchant.java
+++ Merchant.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 //NAMA  : FIKRIYA RACHMA DEWI
 //NIM   : 215150600111008
 public class Merchant {
